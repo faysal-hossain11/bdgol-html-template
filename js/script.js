97be4e9d96959525b5af5d52d116fba6ac4c2c64
faysal <devfaysalbd@gmail.com>
2024-04-26 18:24:15 +0600

    some update

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$('.testimonials-wrap').slick({
+    centerMode: true,
+    // centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 4000,
+
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
