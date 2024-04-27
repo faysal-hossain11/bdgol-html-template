ff0e22eb467efa86f79c1a301f9eaf29932733a7
faysal <devfaysalbd@gmail.com>
2024-04-27 23:08:05 +0600

    some update

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $('.testimonials-wrap').slick({
     // centerPadding: '60px',
     slidesToShow: 3,
     slidesToScroll: 1,
-    // autoplay: true,
-    autoplaySpeed: 4000,
+    autoplay: true,
+    autoplaySpeed: 3000,
 
     responsive: [
         {
@@ -32,9 +32,7 @@ $('.testimonials-wrap').slick({
 
 // start the modal js code
 $(document).on("click", "#cust_btn", function () {
-
     $("#myModal").modal("toggle");
-
 });
 
 //choose-slider
@@ -79,6 +77,7 @@ $('.recent-country-slider').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 4,
+    dots: true,
     slidesToScroll: 1,
     centerPadding: 40,
     arrows: true,
@@ -161,6 +160,82 @@ $('.news-section-slider').slick({
     prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
     nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
 
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+//  new-country-slider
+$('.new-country-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerPadding: 40,
+    arrows: true,
+    prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+//  category-similar-slider
+$('.category-similar-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerPadding: 40,
+    arrows: true,
+    prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
     responsive: [
         {
             breakpoint: 1024,
