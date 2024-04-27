385256eac44dda39bca1ee807d06af0b623179ac
faysal <devfaysalbd@gmail.com>
2024-04-27 09:53:14 +0600

    some update

--- js/script.js
+++ js/script.js
@@ -31,8 +31,159 @@ $('.testimonials-wrap').slick({
 
 
 // start the modal js code
-$(document).on("click","#cust_btn",function(){
-  
+$(document).on("click", "#cust_btn", function () {
+
     $("#myModal").modal("toggle");
-    
-  })
+
+});
+
+//choose-slider
+$('.choose-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+//   recent-country-slider
+$('.recent-country-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerPadding: 40,
+    arrows: true,
+    // prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
+    // nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+//   featured-card-slider
+$('.featured-card-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerPadding: 40,
+    arrows: true,
+    prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+//   news-section-slider
+$('.news-section-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerPadding: 40,
+    arrows: true,
+    prevArrow: '<button class="pagination-btn pagination-left"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="pagination-btn pagination-right"><i class="fa-solid fa-angle-right"></i></button>',
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
